e9aa23fdf839faeb7db2620e6a1c9ec4483e227c
Akshay Kumar <akshaykumar0416@gmail.com>
2023-03-31 11:53:27 +0530

    movix web app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          earum doloremque ea error delectus eligendi nisi reprehenderit cum?
+          Dicta tempora minima possimus numquam nobis, dolorum hic qui eos
+          obcaecati tempore. Eligendi necessitatibus ea quaerat cumque debitis,
+          fuga officiis optio recusandae eaque!
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
